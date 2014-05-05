255234cbbd27d029f9a1745e81a616b9202f0397
neil patel <neil@myindianweddingapp.com>
2014-05-05 17:52:32 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+== README
+
+This is a cool one page landing page anyone can start using today
+
+
+* Ruby version 4.0.4
+
+* Rails 2.0
+
+* Dropbox API
+
+* Bootstrap 3.0
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Landingpage::Application.config.secret_key_base = 'e3cb63189f06079d66ff7825e2d4656e33234bfb99eee1a2b605bcc07c6157c72ee89cf30f9150c50c5e9d8df0af1ec0f784428140d8e48b25069602cee98e82'
